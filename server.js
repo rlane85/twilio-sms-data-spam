5adb31cb1ce8d3750e9612177c7071e3a7b36ad6
Ryan Lane <ryanlane1985@gmail.com>
2019-06-16 16:56:57 -0400

    commit

--- server.js
+++ server.js
@@ -124,13 +124,16 @@ powered by darksky.net`);
   else if (text.includes('current'))
   {
     request(config.WU_OPTIONS, (err, response, data) => {})
+    .then(function(wind) {
+    wind = toCard(data.observations[0].winddir)
+    })
     .then(function(data) {
       twiml.message(`
 Current from WU Station ${data.observations[0].stationID}
 Temp: ${data.observations[0].imperial.temp}
 Heat Index: ${data.observations[0].imperial.heatIndex}
 Wind Speed: ${data.observations[0].imperial.windSpeed} mph
-Wind Direction: ${toCard(data.observations[0].winddir)}(${data.observations[0].winddir})
+Wind Direction: ${wind}(${data.observations[0].winddir})
 Rain Today: ${data.observations[0].imperial.precipTotal} inches
 Pressure: ${data.observations[0].imperial.pressure} inHg
 Humidity: ${data.observations[0].humidity}%`);
